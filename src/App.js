8a51bfbc16e14f6cfe4eaa7001b0d1b3b61d05fd
Jacob Chademwiri <jchademwiri@gmail.com>
2021-09-12 22:49:15 +0200

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
@@ -12,6 +13,18 @@ import ScrollToTop from './components/ScrollToTop';
 import Copyright from './components/Copyright';
 
 function App() {
+	useEffect(() => {
+		ReactGA.initialize('UA-207433899-1');
+		// To Report Page View
+		ReactGA.pageview(window.location.pathname + window.location.search);
+	}, []);
+
+	useEffect(() => {
+		ReactGA.initialize('G-9ES9KKD4KS');
+		// To Report Page View
+		ReactGA.pageview(window.location.pathname + window.location.search);
+	}, []);
+
 	return (
 		<>
 			<Router>
